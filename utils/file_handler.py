f69f432d6632b8d7ebc2c9c6e974e65d701361f4
Pragati Naik <pragatinaik2308@gmail.com>
2026-01-15 21:43:46 +0530

    [Part 1] Parsed raw data and handled data quality issues

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -33,3 +33,37 @@ def read_sales_data(filename):
 
     return cleaned_lines
 
+
+def parse_transactions(raw_lines):
+    parsed_data = []
+
+    for line_no, line in enumerate(raw_lines, start=1):
+        try:
+            # Split by pipe delimiter
+            part_data = line.split('|')
+
+            # Skip rows with incorrect number of fields
+            if len(part_data) !=8:
+                continue
+
+            transaction_id, date, product_id, product_name, quantity, unit_price, customer_id, region = part_data
+            
+            #  Handle Product Name (remove commas) and numeric fields ()
+            record = {
+                "TransactionID": transaction_id.strip(),
+                "Date": date.strip(),
+                "ProductID": product_id.strip(),
+                "ProductName": product_name.replace(",", " ").strip(),
+                "Quantity": int(quantity.replace(",", "").strip()), #convert to int 
+                "UnitPrice": float(unit_price.replace(",", "").strip()), #convert to float
+                "CustomerID": customer_id.strip(),
+                "Region": region.strip()
+            }
+
+            parsed_data.append(record)
+
+        except Exception as e:
+            print(f"Line {line_no} skipped due to unexpected error: {e}")
+
+    return parsed_data
+
